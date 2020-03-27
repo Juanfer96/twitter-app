b598a630401dfc9ddc30284af78a7dae3b476bb8
juanpico12 <juanfernandopico12@gmail.com>
2020-03-27 18:50:54 -0300

    Main components/services added and routing-navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { SearchPageComponent } from './components/search-page/search-page.component';
+import { SettingsPageComponent } from './components/settings-page/settings-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomePageComponent },
+  { path: 'search', component: SearchPageComponent },
+  { path: 'settings', component: SettingsPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TweetComponent } from './components/tweet/tweet.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { TweetDetailComponent } from './components/tweet-detail/tweet-detail.component';
+import { TrendComponent } from './components/trend/trend.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { SearchPageComponent } from './components/search-page/search-page.component';
+import { SettingsPageComponent } from './components/settings-page/settings-page.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TweetComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavbarComponent,
+    TweetDetailComponent,
+    TrendComponent,
+    HomePageComponent,
+    SearchPageComponent,
+    SettingsPageComponent,
   ],
   imports: [
     BrowserModule,
