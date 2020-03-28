8b54fbe2d2bc43e0f7c3466fdaa6d4523ba19fd2
Mauricio Navarro <navarromauricioj@gmail.com>
2020-03-28 17:20:23 -0300

    tweet service- new getTweets()

--- src/app/services/tweet-service.service.ts
+++ src/app/services/tweet-service.service.ts
@@ -1,9 +1,21 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { map, catchError } from "rxjs/operators";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class TweetServiceService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getTweets(): Observable<any> {
+    return this.http.get<any>("http://localhost:8080/timeline?count=100").pipe(
+      map(data => data),
+      catchError(err => {
+        console.log(err);
+        return err;
+      })
+    );
+  }
 }
