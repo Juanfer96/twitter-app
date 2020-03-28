286429c576dd181d9fc28ac144df414bde4c9ae8
Mauricio Navarro <navarromauricioj@gmail.com>
2020-03-28 18:13:00 -0300

    add route from home to tweet detail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,19 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { HomePageComponent } from './components/home-page/home-page.component';
-import { SearchPageComponent } from './components/search-page/search-page.component';
-import { SettingsPageComponent } from './components/settings-page/settings-page.component';
-
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomePageComponent } from "./components/home-page/home-page.component";
+import { SearchPageComponent } from "./components/search-page/search-page.component";
+import { SettingsPageComponent } from "./components/settings-page/settings-page.component";
+import { TweetDetailComponent } from "./components/tweet-detail/tweet-detail.component";
 
 const routes: Routes = [
-  { path: 'home', component: HomePageComponent },
-  { path: 'search', component: SearchPageComponent },
-  { path: 'settings', component: SettingsPageComponent },
+  { path: "home", component: HomePageComponent },
+  { path: "search", component: SearchPageComponent },
+  { path: "settings", component: SettingsPageComponent },
+  { path: "TweetDetail/:id", component: TweetDetailComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
