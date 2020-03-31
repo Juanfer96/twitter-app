db0798a19edc7b3f86d6381e196c2a55aef9939c
chinosoft <chinosoft@gmail.com>
2020-03-31 19:44:15 -0300

    no fix

--- src/app/components/tweet-detail/tweet-detail.component.ts
+++ src/app/components/tweet-detail/tweet-detail.component.ts
@@ -63,7 +63,7 @@ export class TweetDetailComponent implements OnInit {
     this.getId();
     this.ts.getIdTweet(this.tweetId)
     .subscribe( data => {
-      this.Tweet = data.statuses[0];
+      this.Tweet = data[0].statuses;
 
       console.log(this.Tweet);
       console.log(data);
