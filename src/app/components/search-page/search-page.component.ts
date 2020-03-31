1d1e08a9dc1d15e41f227f5ca7dfcbd1c97e4c65
juanpico12 <juanfernandopico12@gmail.com>
2020-03-31 01:41:00 -0300

    Using tweet component to display search timeline with infinite scroll

--- src/app/components/search-page/search-page.component.ts
+++ src/app/components/search-page/search-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { TweetServiceService } from "src/app/services/tweet-service.service";
 import { ITweet } from 'src/app/interfaces/tweetInterface';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { NgxSpinnerService } from "ngx-spinner";
 
 @Component({
   selector: 'app-search-page',
@@ -14,8 +15,15 @@ export class SearchPageComponent implements OnInit {
   faSearch = faSearch;
   searching :boolean;
 
+  notscrolly :boolean = true;
+  allTweetsLoaded :boolean = false;
+  initialTimeLineTweets :number = 50; //the initial tweets that are going to we displayed
+  index :number = 1; //the index used to keep displaying tweets in the timeline
+  cont :number; //used to count the tweets on the timeline
+
   constructor(
-     public ts: TweetServiceService
+     public ts: TweetServiceService,
+     private spinner: NgxSpinnerService,
   ) { }
 
   ngOnInit(): void {
@@ -44,6 +52,28 @@ export class SearchPageComponent implements OnInit {
     })
   }
 
+  onScroll() {
+    this.spinner.show(); //shows the spinner while the tweets are loading
+    if (this.cont >= 200) {
+      //when all the maximum number of tweets are loaded,a message is displayed in the html of the component
+      this.allTweetsLoaded = true;
+    }
+
+    if (this.notscrolly) {
+      this.notscrolly = false;
+      this.index++;
+      this.cont = this.initialTimeLineTweets * this.index;
+      this.addNewTeetsTimeLine(this.cont);
+    }
+  }
+
+  addNewTeetsTimeLine(count: number) {
+    this.ts.getTweets(count).subscribe(tweets => {
+      this.tweets = tweets;
+      this.notscrolly = true;
+      this.spinner.hide(); //hides the spinner when the time line is loaded
+    });
+  }
 }
 
 
