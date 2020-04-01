20f810a20146691828214121bde61470c4808f95
chinosoft <chinosoft@gmail.com>
2020-03-31 22:16:42 -0300

    fix 44

--- src/app/components/tweet/tweet.component.ts
+++ src/app/components/tweet/tweet.component.ts
@@ -1,11 +1,5 @@
 import { Component, OnInit, Input } from "@angular/core";
 import { ITweet } from "src/app/interfaces/tweetInterface";
-<<<<<<< HEAD
-
-=======
-import { faHeart } from '@fortawesome/free-regular-svg-icons';
-import { faRetweet } from '@fortawesome/free-solid-svg-icons';
->>>>>>> 02c86123aaff702f9c1dad109e193fd616e2199f
 @Component({
   selector: "app-tweet",
   templateUrl: "./tweet.component.html",
@@ -13,11 +7,6 @@ import { faRetweet } from '@fortawesome/free-solid-svg-icons';
 })
 export class TweetComponent implements OnInit {
   @Input() Tweets: ITweet[];
-<<<<<<< HEAD
-=======
-  faHeart = faHeart;
-  faRetweet = faRetweet;
->>>>>>> 02c86123aaff702f9c1dad109e193fd616e2199f
   constructor() {}
 
   ngOnInit(): void {}
--- src/app/services/tweet-service.service.ts
+++ src/app/services/tweet-service.service.ts
@@ -9,11 +9,8 @@ import { ITweet } from '../interfaces/tweetInterface';
 })
 export class TweetServiceService {
   private searchURL = 'http://localhost:8080/search?q=';
-<<<<<<< HEAD
   private searchId = 'http://localhost:8080/show?id=';
   
-=======
->>>>>>> 02c86123aaff702f9c1dad109e193fd616e2199f
   constructor(private http: HttpClient) {}
 
   getTweets(tweetsToDisplay: number): Observable<any> {
@@ -28,7 +25,6 @@ export class TweetServiceService {
       );
   }
 
-<<<<<<< HEAD
   getSearchTweets (searchText : string): Observable<any> {
     return this.http.get<any>(this.searchURL+searchText)
     .pipe(
@@ -41,12 +37,6 @@ export class TweetServiceService {
     return this.http.get<any>(this.searchURL+searchId)
     .pipe(
       catchError(this.handleError<ITweet[]>('getIdTweet', []))
-=======
-  getSearchTweets (searchText : string,count :number): Observable<any> {
-    return this.http.get<any>(this.searchURL+searchText+'&count='+count)
-    .pipe(
-      catchError(this.handleError<ITweet[]>('getSearchTweets', []))
->>>>>>> 02c86123aaff702f9c1dad109e193fd616e2199f
     );
   }
 
